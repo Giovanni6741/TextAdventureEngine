0073c5ffb5428625bb45649613ab5c6d3e2d06b4
Giovanni6741 <galeazzigiovannibenedetto@unclickallavolta.com>
2026-09-05 11:36:50 +0200

    Creata la Classe State

---
+++ src/textadventure/state.py
@@ -0,0 +1,7 @@
+class State:
+    def __init__(self):
+        # Qui puoi mettere variabili di stato del gioco
+        self.data = {}
+
+        # Esempio: scena di fallback
+        self.default_next_scene = None
