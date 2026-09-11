1e3f2c964d8114fcf1ac4cb4101db0bf4d1a35c0
Giovanni6741 <galeazzigiovannibenedetto@unclickallavolta.com>
2026-09-11 23:10:25 +0200

    Primo commit dev

--- src/textadventure/__init__.py
+++ src/textadventure/__init__.py
@@ -2,5 +2,10 @@ from .engine import GameEngine
 from .scene import Scene
 from .parser import Parser
 from .state import State
+from .scene_draw import SceneDraw
+from .map_draw import MapDraw
+from .hud import HUD
+from .inventory_draw import InventoryDraw
+from .utils import box
 
-__all__ = ["GameEngine", "Scene", "Parser", "State"]
+__all__ = ["GameEngine", "Scene", "Parser", "State", "SceneDraw", "MapDraw", "HUD", "InventoryDraw", "box"]
